0804b07b156f449874da4caed6301a417bb31086
Akash-YS05 <akash.pan05@gmail.com>
2025-03-12 23:15:38 +0530

    bridging base

--- src/BridgeBase.sol
+++ src/BridgeBase.sol
@@ -6,18 +6,37 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { console } from "forge-std/console.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
+interface IBUSDT is IERC20 {
+    function mint(address _to, uint256 _amount) external;
+    function burn(address _form, uint256 _amount) external;
+}
+
 contract BridgeBase is Ownable {
-    constructor() Ownable(msg.sender) {
+    uint256 public balance;
+    address public tokenAddress;
 
+    event Burn(address indexed burner, uint amount);
+
+    mapping(address => uint256) public pendingBalance;
+
+    //_tokenAddress is the bridge token address
+    constructor(address _tokenAddress) Ownable(msg.sender) {
+        tokenAddress = _tokenAddress;
     }
 
-    function withdraw() public {
-
+    function burn(IBUSDT _tokenAddress, uint256 _amount) public {
+        require(address(_tokenAddress) == tokenAddress);
+        _tokenAddress.burn(msg.sender, _amount);
+        emit Burn(msg.sender, _amount);
     }
-    function burn() public {
 
+    function withdraw(IBUSDT _tokenAddress, uint256 _amount) public {
+        require(pendingBalance[msg.sender] >= _amount);
+        pendingBalance[msg.sender] -= _amount;
+        _tokenAddress.mint(msg.sender, _amount);
     }
-    function depositEventOnOtherSide() public {
 
+    function depositEventOnOtherSide(address userAccount, uint256 _amount) public onlyOwner {
+        pendingBalance[userAccount] += _amount;
     }
 }
